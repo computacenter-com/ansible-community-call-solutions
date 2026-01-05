711a2ee3de455d05295b003f6dbc9caf9318c800
Tim Grützmacher <tim.gruetzmacher@computacenter.com>
2026-01-05 18:33:45 +0100

    Renamed folders. Updated link to Readme files.

---
+++ 2025-08-12-demo-advanced-role-usage/export-aci-credentials.sh
@@ -0,0 +1,5 @@
+# Get access details here: https://devnetsandbox.cisco.com/DevNet/catalog/ACI-Simulator-Always-On_aci-simulator-always-on
+export ACI_HOST=''
+export ACI_USERNAME=''
+export ACI_PASSWORD=''
+export ACI_VALIDATE_CERTS=false
---
+++ 2025-10-14-demo-migrate-roles-to-collections/collections/ansible_collections/computacenter/demo/README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - computacenter.demo
+
+Documentation for the collection.
